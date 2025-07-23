39abd13c01637299472716ef383935daa163554b
Dev.Venkat <32322499+Enginnervenkat@users.noreply.github.com>
2025-07-23 22:48:04 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,18 @@
+import React from "react";
+import Header from "./components/Header";
+import Section from "./components/Section";
+import ProjectCards from "./components/ProjectCards";
+import Footer from "./components/Footer";
+
+function App() {
+  return (
+    <div>
+      <Header />
+      <Section />
+      <ProjectCards />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
